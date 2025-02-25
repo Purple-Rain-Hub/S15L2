dca0e021f4d1f2528e54d8e0ca787c0503df5cee
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-02-25 15:06:24 +0100

    Add files via upload

---
+++ S15L2/Esercizio2.sql
@@ -0,0 +1,23 @@
+-- 1)
+SELECT * FROM Products;
+
+-- 2)
+SELECT * FROM Products WHERE (UnitsInStock >= 40);
+
+--3)
+SELECT * FROM Employees WHERE (City = 'London');
+
+--4)
+SELECT * FROM Orders ORDER BY Freight DESC;
+
+--5)
+SELECT * FROM Orders WHERE (Freight >90 AND Freight <200);
+
+--6)
+SELECT * FROM Categories WHERE (CategoryID = 1);
+
+--7)
+SELECT * FROM [Order Details] WHERE (Discount >0);
+
+--8)
+SELECT * FROM Orders WHERE (CustomerID = 'BOTTM' AND Freight > 50);
